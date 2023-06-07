fc2b018806d2b0210f86a23d84256a7f1056b1dc
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2023-06-06 22:38:57 -0700

    added nav side menu and logo

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,12 +4,18 @@ import axios from 'axios';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Button from 'react-bootstrap/Button';
 import { motion } from 'framer-motion';
 
 
 const NavBar = ({loggedIn, setLoggedIn}) => {
     const navigate = useNavigate();
     const [user, setUser] = useState(null);
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     const [windowWidth, setWindowWidth] = useState(window.innerWidth)
     const [windowHeight, setWindowHeight] = useState(window.innerHeight)
     const setWindowDimensions = () => {
@@ -52,22 +58,37 @@ const NavBar = ({loggedIn, setLoggedIn}) => {
         <Container>
           <Navbar.Brand as={Link} to="/"><img
                 alt=""
-                src="logo192.png"
-                width="30"
+                src="smooth.png"
+                width="50"
                 height="30"
-                className="d-inline-block align-top"
-              />{' '}</Navbar.Brand>
+                className="d-inline-block align-top hide-bg"
+              />{' '}francois</Navbar.Brand>
                 {windowWidth<640?(
+                  
                   <Nav className=" justify-contend-end">
-                  <Nav.Link as={Link} to="/login"><i className="bi bi-list"></i></Nav.Link>
+                    <Nav.Link as={Link}><i className="bi bi-list" onClick={handleShow}></i></Nav.Link>
+                      <Offcanvas placement='end' show={show} onHide={handleClose}>
+                        <Offcanvas.Header closeButton>
+                          <Offcanvas.Title>Products</Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                    <Nav className=" justify-contend-end">
+                      <Nav.Link style={{fontSize:'large'}} as={Link} to="/face">Face</Nav.Link>
+                      <Nav.Link style={{fontSize:'large'}} as={Link} to="/body">Body</Nav.Link>
+                      <Nav.Link style={{fontSize:'large'}} as={Link} to="/wellness">Wellness</Nav.Link>
+                    </Nav>
+                  </Offcanvas.Body>
+                </Offcanvas>
+
+
                   <Nav.Link as={Link} to="/register"><i className="bi bi-search"></i></Nav.Link>
                   <Nav.Link as={Link} to="/cart"><i className="bi bi-bag"></i><i className="bi bi-1-circle-fill"></i></Nav.Link>
                 </Nav>
                 ):(
                   <Nav className=" justify-contend-end">
-                  <Nav.Link style={{fontSize:'small'}} as={Link} to="/login">Face</Nav.Link>
-                  <Nav.Link style={{fontSize:'small'}} as={Link} to="/login">Body{windowWidth}</Nav.Link>
-                  <Nav.Link style={{fontSize:'small'}} as={Link} to="/login">Wellness</Nav.Link>
+                  <Nav.Link style={{fontSize:'small'}} as={Link} to="/login" className='d-flex align-items-center'>Face</Nav.Link>
+                  <Nav.Link style={{fontSize:'small'}}  as={Link} to="/login"className='d-flex align-items-center'>Body</Nav.Link>
+                  <Nav.Link style={{fontSize:'small'}} as={Link} to="/login" className='d-flex align-items-center'>Wellness</Nav.Link>
                   <Nav.Link as={Link} to="/register"><i className="bi bi-search"></i></Nav.Link>
                   <Nav.Link as={Link} to="/cart"><i className="bi bi-bag"></i><i className="bi bi-1-circle-fill"></i></Nav.Link>
                 </Nav>
