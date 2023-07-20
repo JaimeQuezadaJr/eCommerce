bd91891717a1cb236562562fe5cb904955f4c5eb
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2023-07-19 23:07:53 -0700

    added useeffect for instant top page scroll

--- client/src/components/BuyProduct.js
+++ client/src/components/BuyProduct.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Product from './Product'
 
 const BuyProduct = () => {
+
+  useEffect(() => {
+    window.scrollTo({top:0, left:0, behavior:'instant'})
+  },[])
+  
   return (
     <div>
         <Product></Product>
