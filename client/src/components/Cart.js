2dfdb9e0db810a31cc429494881d3ec5cb1f648e
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2023-07-20 22:56:23 -0700

    added instant top page scroll with useeffect

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 const Cart = () => {
+
+  useEffect(() => {
+    window.scrollTo({top:0, left:0, behavior:'instant'})
+  },[])
+  
   return (
     <div>Cart</div>
   )
