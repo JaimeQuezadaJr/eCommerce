aa69aecfc3b8e244a0c5f4bbe1746f541859aca9
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2023-07-24 23:25:45 -0700

    added resize window hooks

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,11 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
+  const navigate = useNavigate();
+  const { scrollYProgress } = useScroll();
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight)
+  const setWindowDimensions = () => {
+    setWindowWidth(window.innerWidth)
+    setWindowHeight(window.innerHeight)
+  }
+
+  useEffect(() => {
+      window.addEventListener('resize', setWindowDimensions);
+      return () => {
+        window.removeEventListener('resize', setWindowDimensions)
+      }
+    }, [])
 
   useEffect(() => {
     window.scrollTo({top:0, left:0, behavior:'instant'})
   },[])
-  
+
   return (
     <div>Cart</div>
   )
