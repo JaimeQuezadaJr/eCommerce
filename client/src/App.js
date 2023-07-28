19ff11e9c69cf20f8af9af10266464a9a99f3270
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2023-07-27 22:27:41 -0700

    removed imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import { Routes, Route, useLocation} from 'react-router-dom';
-import { useState } from 'react';
+
 import Home from './components/Home';
 import NavBar from './components/NavBar';
-import Product from './components/Product';
+
 import { AnimatePresence } from 'framer-motion';
 import ProductInfo from './components/ProductInfo';
 import BuyProduct from './components/BuyProduct';
