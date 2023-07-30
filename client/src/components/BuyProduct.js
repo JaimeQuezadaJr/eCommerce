179a2b13f8f761b640ac566cda22950b0224901e
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2023-07-29 22:24:13 -0700

    added page sizing

--- client/src/components/BuyProduct.js
+++ client/src/components/BuyProduct.js
@@ -1,7 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Product from './Product'
 
 const BuyProduct = () => {
+  const navigate = useNavigate();
+  const { scrollYProgress } = useScroll();
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight)
+  const setWindowDimensions = () => {
+    setWindowWidth(window.innerWidth)
+    setWindowHeight(window.innerHeight)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', setWindowDimensions);
+    return () => {
+      window.removeEventListener('resize', setWindowDimensions)
+    }
+  }, [])
 
   useEffect(() => {
     window.scrollTo({top:0, left:0, behavior:'instant'})
