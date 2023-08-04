673a8e975fb1137edd42222f22f696f64c9818f2
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2023-08-04 09:35:58 -0700

    added usescroll framer motion import

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { motion, useScroll } from 'framer-motion';
 
 const Cart = () => {
   const navigate = useNavigate();
